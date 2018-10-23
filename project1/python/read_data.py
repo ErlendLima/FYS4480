7fd258014fbea7cb7cf7a1c89e2790f6ed621cf9
Erlend Lima <erlendlima@outlook.com>
2018-10-23 18:06:32 +0200

    Add tiny pep8 corrections

--- project1/python/read_data.py
+++ project1/python/read_data.py
@@ -9,33 +9,34 @@ class MatrixElementParser(object):
     """
     Reads matrix element expressions from file and stores as a pandas
     dataframe of sympy expressions, which can be evaluated for
-    different Z with i.e self.eval_data(Z=2). 
+    different Z with i.e self.eval_data(Z=2).
     """
 
-    def __init__(self, filename = '../data/matrix_data.txt', Z = 1):
+    def __init__(self, filename='../data/matrix_data.txt', Z=1):
         self.filename = filename
         self.sympy_data = self.read_data(filename)
-        self.Z = 1 # invokes property
+        self.Z = 1  # invokes property
 
     def __getitem__(self, ind):
-        """ 
+        """
         Gets matrix element <pq|V|rs>_AS from ind = [p,q,r,s], assuming
-        p,q,r,s \in {0,...,5} with odd and even integers corresponding to
+        p,q,r,s ∈ {0,...,5} with odd and even integers corresponding to
         opposite spins, and (p//2 + 1) as the energy levels 1s, 2s, 3s.
         """
         ind = np.array(ind)
+        if np.any(ind < 0) or np.any(5 < ind):
+            raise IndexError("Index out of range.")
 
         # spin indices
-        s1,s2,s3,s4 = ind % 2 
+        s1, s2, s3, s4 = ind % 2
         # spatial indices
-        r1,r2,r3,r4 = ind // 2 + 1
+        r1, r2, r3, r4 = ind // 2 + 1
 
         matr = self.matrix
-        
-        mel = matr.loc[(r1,r2),(r3,r4)] * ((s1==s3) and (s2 == s4)) \
-            - matr.loc[(r1,r2),(r4,r3)] * ((s1==s4) and (s2 == r3))
-        return mel
 
+        mel = matr.loc[(r1, r2), (r3, r4)] * ((s1 == s3) and (s2 == s4)) \
+            - matr.loc[(r1, r2), (r4, r3)] * ((s1 == s4) and (s2 == r3))
+        return mel
 
     @staticmethod
     def read_data(filename):
@@ -45,23 +46,23 @@ class MatrixElementParser(object):
         """
         sympy_data = pd.DataFrame()
         with open(filename) as infile:
-            lines = infile.read()
-            for line in lines.split('\n'):
-                if not line: continue
+            for line in infile.read().split('\n'):
+                if not line:
+                    continue
                 spl = line.split()
                 i1, i2 = spl[:2]
                 s = sympy_parser.parse_expr(spl[-1])
-                sympy_data.loc[i1,i2] = s
+                sympy_data.loc[i1, i2] = s
         return sympy_data
-    
-    def eval_data(self, Z=None, index_type = 'multiindex'):
+
+    def eval_data(self, Z=None, index_type='multiindex'):
         """
         Evaluates matrix elements for a given Z, defaulting to given Z. Returns pandas DataFrame.
         """
         Z = Z or self.Z
         Z_symb = sp.symbols('Z')
 
-        m = self.sympy_data.applymap(lambda s:s.evalf(subs = {Z_symb:Z}))
+        m = self.sympy_data.applymap(lambda s: s.evalf(subs={Z_symb: Z}))
 
         # m = matr_parse.sympy_data.applymap(lambda s:s.evalf(subs = {Z_symb:Z}))
         if index_type == 'multiindex':
@@ -70,12 +71,12 @@ class MatrixElementParser(object):
             row1, row2 = m.index.str
             row1, row2 = row1.astype('int'), row2.astype('int')
 
-            row_ind = pd.MultiIndex.from_arrays([row1,row2])
-            col_ind = pd.MultiIndex.from_arrays([col1,col2])
-            m.index   = row_ind
+            row_ind = pd.MultiIndex.from_arrays([row1, row2])
+            col_ind = pd.MultiIndex.from_arrays([col1, col2])
+            m.index = row_ind
             m.columns = col_ind
         return m.astype('float')
-    
+
     @property
     def matrix(self):
         """Returns matrix"""
@@ -94,8 +95,6 @@ class MatrixElementParser(object):
     def Z(self, Z):
         self._Z = Z
 
-
-
     def show_matrix(self):
         """
         Plots the values of the matrix, for Z = 1.
@@ -103,27 +102,23 @@ class MatrixElementParser(object):
         import numpy as np
         import matplotlib.pyplot as plt
         matr = self.eval_data(1)
-        fig,ax = plt.subplots(1)
+        fig, ax = plt.subplots()
 
-        m = plt.imshow(np.array(matr.values, dtype = np.float64))
+        m = ax.matshow(np.array(matr.values, dtype=np.float64))
 
-        ax.set_yticks(np.arange(matr.shape[0]), minor = False)
-        ax.set_xticks(np.arange(matr.shape[1]), minor = False)
-        ax.set_yticklabels(matr.columns, minor = False)
-        ax.set_xticklabels(matr.index, minor = False)
-        ax.xaxis.tick_top()
+        ax.set_yticks(np.arange(matr.shape[0]), minor=False)
+        ax.set_xticks(np.arange(matr.shape[1]), minor=False)
+        ax.set_yticklabels(matr.columns, minor=False)
+        ax.set_xticklabels(matr.index, minor=False)
 
-        cax = plt.colorbar(m)#, orientation='horizontal')
+        cax = fig.colorbar(m, fraction=0.046, pad=0.04)
         cax.set_label('Units of $Z$')
         plt.show()
 
-    
-
-
-
-        
 
 if __name__ == "__main__":
     a = MatrixElementParser()
-    print(a.eval_data(1))
-    a.show_matrix()
+    # print(a.eval_data(1))
+    # a.show_matrix()
+    # print(a.matrix)
+    print(a[2, 2, 2, 2])
